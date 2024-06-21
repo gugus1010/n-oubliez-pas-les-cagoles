5a8f720eb7e578f3c7b17e3146bf47a8f075d831
gugus1010 <gurwan.ledref@gmail.com>
2024-06-21 16:35:17 +0200

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -3,17 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000; // Utilisation du port fourni par l'environnement ou 3000 par défaut
+const port = process.env.PORT || 3000;
 
 let responses = [];
 
 app.use(bodyParser.json());
-app.use(cors()); // Active CORS pour toutes les routes
+app.use(cors());
 
+// Endpoint pour enregistrer une réponse
 app.post('/save-response', (req, res) => {
     const { artist, track } = req.body;
     if (artist && track) {
-        // Gestion de l'ID simulée pour le leaderboard
         const id = responses.length + 1;
         responses.push({ id, artist, track });
         res.status(200).json({ message: 'Réponse enregistrée avec succès !' });
@@ -22,6 +22,7 @@ app.post('/save-response', (req, res) => {
     }
 });
 
+// Endpoint pour récupérer le leaderboard
 app.get('/leaderboard', (req, res) => {
     res.json(responses);
 });
